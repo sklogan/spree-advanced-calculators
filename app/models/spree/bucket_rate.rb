13e4efaed4a3fb6a945a3991880b3fcdc46a75df
sklogan <logan.senthilkumar@gmail.com>
2013-11-15 17:41:53 +0530

    upgraded

--- app/models/spree/bucket_rate.rb
+++ app/models/spree/bucket_rate.rb
@@ -1,5 +1,5 @@
 class BucketRate < ActiveRecord::Base
-  belongs_to :calculator
+  belongs_to :calculator, class_name: "Spree::Calculator"
   
   validates_presence_of :floor, :ceiling, :rate, :calculator_id
 
--- lib/spree_advanced_calculators_hooks.rb
+++
@@ -1,7 +0,0 @@
-class SpreeAdvancedCalculatorsHooks < Spree::ThemeSupport::HookListener
-  # custom hooks go here
-  
-  insert_after :admin_configurations_menu do
-    "<%= configurations_menu_item(I18n.t('bucket_rates'), admin_bucket_rates_path, I18n.t('bucket_rates_description')) %>"
-  end
-end
